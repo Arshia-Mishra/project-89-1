8bf96d35621cd2e0fd44e63544a21223db8ce610
Arshia-Mishra <80540337+Arshia-Mishra@users.noreply.github.com>
2021-08-20 17:02:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function nextpage()
+{
+    window.location="index89.html";
+}
+
+function nextpage2()
+{
+    num1 = document.getElementById("no.1").value;
+    num2 = document.getElementById("no.2").value;
+
+    localStorage.setItem("num1",num1);
+    localStorage.setItem("num2",num2);
+    
+    window.location="index90.html";
+}
+
+function nextpage3()
+{
+    document.getElementById("num1","x","num2")
+
+    localStorage
+    window.location="index89.html";
+}
